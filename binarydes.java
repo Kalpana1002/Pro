02e756e174cea85aae3bae49c45b752fbbe87cb6
S Kalpana <kalpanasathish2@gmail.com>
2017-06-13 12:01:39 +0530

    binarydes.java

--- binarydes.java
+++ binarydes.java
@@ -4,27 +4,30 @@ class binarydes
 {
     public static void main(String args[])
     {
-    int m,sum=0;
         Scanner scan=new Scanner(System.in);
         int n=scan.nextInt();
         int num[]=new int[n];
-        String bi[]=new String[n];
-        int des[]=new int[n];
+        int bi[]=new int[n];
+        int di[]=new int[n];
         for(int inp=0;inp<n;inp++)
         {
             num[inp]=scan.nextInt();
+            String s=Integer.toBinaryString(num[inp]);
+            bi[inp]=s.length()-s.replaceAll("1","").length();
         }
-        for(int i=0;i<num.length;i++)
+        System.arraycopy(bi,0,di,0,bi.length);
+        Arrays.sort(bi);
+        for(int i=n-1;i>=0;i--)
         {
-            bi[i]=Integer.toBinaryString(num[i]);
-            while(bi[i]>0)
+            for(int j=0;j<n;j++)
             {
-            m=bi[i]%10;
-            sum=sum+m;
-            bi[i]=bi[i]/10;
+                if(bi[i]==di[j] && num[j]!=0)
+                {
+                    System.out.println(num[j]);
+                    num[j]=0;
+                    break;
+                }
             }
-            des[i]=sum;
-   System.out.println(des[i]);
         }
         
     }
