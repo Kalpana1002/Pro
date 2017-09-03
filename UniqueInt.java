1a70babb91a5acbdcc85c73740a8b4a71a80309e
S Kalpana <kalpanasathish2@gmail.com>
2017-09-03 20:16:46 +0530

    UniqueInt.java

--- UniqueInt.java
+++ UniqueInt.java
@@ -3,17 +3,23 @@ class UniqueInt
 {
 public static void main(String args[])
 {
-Scanner sc = new Scanner(System.in);
-int num[] = new int[args.length];
-int n[] = new int[10000];
-for(int i=0;i<args.length;i++)
-num[i] = Integer.parseInt(args[i]);   //to get Array elements
-for(int i=0;i<num.length;i++)
-n[num[i]] += 1;
-for(int i=0;i<n.length;i++)
-{
-if(n[i]==1)
-System.out.println(i);
-}
+  int flag = 0;
+  Scanner sc = new Scanner(System.in);
+  int num[] = new int[args.length];
+  int n[] = new int[10000];
+  for(int i=0;i<args.length;i++)
+  num[i] = Integer.parseInt(args[i]);   //to get Array elements
+  for(int i=0;i<num.length;i++)
+  n[num[i]] += 1;
+  for(int i=0;i<n.length;i++)
+  {
+    if(n[i]==1)
+    {
+      flag = 1;
+      System.out.println(i);
+    }
+  }
+  if(flag==0)
+    System.out.println("No Unique element");
 }
 }
